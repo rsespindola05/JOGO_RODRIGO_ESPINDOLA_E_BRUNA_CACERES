40b3d79b3efab921fa50ee7ad1e9b6aced384c60
rsespindola05 <110543186+rsespindola05@users.noreply.github.com>
2022-09-16 14:15:13 -0400

    Add files via upload
    Agora foi, terminamos

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $(function(){
-	});
 	var vez = 1;
 	var vencedor = "";
 	
@@ -27,19 +26,20 @@ $(function(){
 			casasIguais(1, 4, 7) || casasIguais(2, 5, 8) || casasIguais(3, 6, 9) ||
 			casasIguais(1, 5, 9) || casasIguais(3, 5, 7)
 			){
-			$("#resultado").html("<h1>O jogador " + vencedor + "venceu! </h1>");
+			$("#resultado").html("<h1>O jogador " + vencedor + " venceu! </h1>");
 			$(".casa").off("click");
 		}
 	}
 	
 	$(".casa").click(function(){
+		console.log("clicou")
 		var bg = $(this).css("background-image");
 		if(bg == "none" || bg == "")
 		{           
-			var fig = "url(" + vez.toString() + ".png)";
+			var fig = "url(" + vez.toString() + ".jpg)";
 			$(this).css("background", fig);
 			vez = (vez == 1? 2:1);  
-			verificarFimDeJogo();
-
+			verificarFimDeJogo(true);
 		}
-	});
+	});	
+});
