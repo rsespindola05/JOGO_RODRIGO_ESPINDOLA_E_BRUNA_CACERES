e7d9bb5a297f0099b8db6ca8f43afaf0561da83b
rsespindola05 <rsespindola05@gmail.com>
2022-09-18 00:31:00 -0400

    Incrementamos o Readme no nosso programa.

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 $(function(){
-	//O conteúdo deve ficar aqui
-	});
 	var vez = 1;
 	var vencedor = "";
 	
@@ -28,18 +26,20 @@ $(function(){
 			casasIguais(1, 4, 7) || casasIguais(2, 5, 8) || casasIguais(3, 6, 9) ||
 			casasIguais(1, 5, 9) || casasIguais(3, 5, 7)
 			){
-			$("#resultado").html("<h1>O jogador " + vencedor + "venceu! </h1>");
+			$("#resultado").html("<h1>O jogador " + vencedor + " venceu! </h1>");
 			$(".casa").off("click");
 		}
 	}
 	
 	$(".casa").click(function(){
+		console.log("clicou")
 		var bg = $(this).css("background-image");
 		if(bg == "none" || bg == "")
 		{           
-			var fig = "url(" + vez.toString() + ".png)";
+			var fig = "url(" + vez.toString() + ".jpg)";
 			$(this).css("background", fig);
 			vez = (vez == 1? 2:1);  
-			verificarFimDeJogo();
+			verificarFimDeJogo(true);
 		}
 	});	
+});
